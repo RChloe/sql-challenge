aac8276441f41a68813e63492ab58446fb685bc6
Rachel-Chloe Gregory <rachel.chloe.gregory@gmail.com>
2019-10-13 20:09:18 -0500

    Completed through #8

--- question_queries.sql
+++ question_queries.sql
@@ -22,8 +22,59 @@ on employees.emp_no=dept_manager.emp_no*/
 
 
 /* 4. List the department of each employee with the following information: 
-employee number, last name, first name, and department name. */
+employee number, last name, first name, and department name.
 
-select employees.emp_no, last_name, first_name, dept_name from employees,dept_emp
-inner join departments
-on departments.dept_no=dept_emp.dept_no
+select departments.dept_name,dept_emp.emp_no,employees.last_name,employees.first_name from departments
+inner join dept_emp
+on dept_emp.dept_no=departments.dept_no
+inner join employees
+on employees.emp_no=dept_emp.emp_no*/
+
+
+/* 5. List all employees whose first name is "Hercules" and last names begin with "B." 
+
+select * from employees
+where first_name = 'Hercules'
+and last_name like 'B%'*/
+
+
+/*6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+select departments.dept_name,dept_emp.emp_no,employees.last_name,employees.first_name from departments
+inner join dept_emp
+on dept_emp.dept_no=departments.dept_no
+inner join employees
+on employees.emp_no=dept_emp.emp_no
+where departments.dept_name = 'Sales'*/
+
+
+/*7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+select departments.dept_name,dept_emp.emp_no,employees.last_name,employees.first_name from departments
+inner join dept_emp
+on dept_emp.dept_no=departments.dept_no
+inner join employees
+on employees.emp_no=dept_emp.emp_no
+where departments.dept_name = 'Sales'
+or departments.dept_name = 'Development'*/
+
+
+/*8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.*/
+
+select COUNT(last_name) as frequency,last_name from employees
+group by last_name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
